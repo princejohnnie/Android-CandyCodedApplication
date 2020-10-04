177280c996c783cfd5ac0e34381b3dbe6e7e7996
princejohnnie <61082393+princejohnnie@users.noreply.github.com>
2020-10-04 16:00:31 +0100

    Added code to launch a Phone Activity in the project

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,6 +34,13 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        phoneIntent.setData(phoneUri);
+        startActivity(phoneIntent);
+    }
+
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
